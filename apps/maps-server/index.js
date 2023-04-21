7773bcc2388085b5608a71660a3ada89fd4ec6aa
Nestor Felipe <necrifede@gmail.com>
2023-04-21 22:08:08 +0200

    feat: configurar el repositorio para utilizarse como monorepositorio
    El repositorio de front-end fue movido a este de backend

--- apps/maps-server/index.js
+++ apps/maps-server/index.js
@@ -5,6 +5,10 @@ dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
+server.get('/healthy', (_, res) => {
+  res.status(200).send({ status: 'ok', message: 'simple healthy response' })
+})
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
